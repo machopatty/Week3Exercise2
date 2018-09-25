e465eabada73a4d5dc9e251ddd255541b716d4ec
student <kump@madisoncollege.edugit config --global user.email kump@madisoncollege.edu>
2018-09-24 22:01:46 -0500

    adding custom tags.

---
+++ customTags/src/main/java/com/markkump/customtags/HelloWorldTag.java
@@ -0,0 +1,18 @@
+package com.markkump.customtags;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+
+public class HelloWorldTag extends SimpleTagSupport {
+    @Override
+    public void doTag() throws JspException, IOException {
+        super.doTag();
+
+        JspWriter out = getJspContext().getOut();
+
+        out.println("Hello Enterprise Java Class");
+
+    }
+}
